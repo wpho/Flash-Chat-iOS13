e44af7b95df6aa3581f1b0566f04f254c8b02117
wpho <u.pho@tcu.edu>
2021-09-01 06:59:46 -0500

    Completed Flash Chat app with data stored in Firebase
    Added models, views & controllers. Imported Firebase.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = K.appName
        
     }
     
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Flash Chat iOS13' do
+  use_frameworks!
+
+  # Pods for Flash Chat iOS13
+  pod 'CLTypingLabel'
+  
+  # add pods for desired Firebase products
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+end
